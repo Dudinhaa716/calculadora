1432783886b81bea948eef69204ac7e32e52c9a2
Dudinhaa716 <aquinomariaeduarda04@gmail.com>
2024-11-22 15:34:16 -0300

    lógica

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ export default function App() {
   //Variavel de estado
   //primeiroValor é a variavel de estado
   //setPrimeiroValor é a função atualizadora
-const [primeiroValor, setPrimeiroValor] =useState()
+const [primeiroValor, setPrimeiroValor] =useState();
 const [segundoValor, setSegundoValor] = useState();
+const [resultado, setResultado] = useState();
 
 //arrow function () => {}
 // funçao especial
@@ -18,17 +19,36 @@ const capturandoPrimeiroValor = (e) => {
 const capturandoSegundoValor = (e) => {
   setSegundoValor(Number(e.target.value))
 }
+
+//função somar
+const soma = () => {
+  setResultado(primeiroValor + segundoValor)
+}
+//função de subtração
+const subtraçao = () => {
+  setResultado(primeiroValor - segundoValor)
+}
+//função de divisão
+const divisao = () => {
+  setResultado(primeiroValor / segundoValor)
+}
+//função de multiplicaçao
+const multiplicaçao = () => {
+  setResultado(primeiroValor * segundoValor)
+}
+
   return (
     <main>
       <h1>Calculadora</h1>
       <input onChange={capturandoPrimeiroValor} type="number" placeholder="insira um número"/>
       <input onChange={capturandoSegundoValor} type="number" placeholder="insira um número"/>
       <div>
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
-        <button>x</button>
+        <button onClick={soma}>+</button>
+        <button onClick={subtraçao}>-</button>
+        <button onClick={divisao}>/</button>
+        <button onClick={multiplicaçao}>x</button>
       </div>
+      <h3>{resultado}</h3>
     </main>
   )
 }
